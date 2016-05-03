e407ed29c22aaa0da2aa1c145d0cbd0ef96bdc06
Morasthite <Morasthite@gmail.com>
2016-05-03 16:19:40 -0700

    dynamic creation done

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //***************************** GLOBAL VARIABLES  *******************************//
 var player1_name_value;
 var player2_name_value;
-var num_of_rows;
-var num_of_cells_to_win;
+var num_of_rows = 9;
+var num_of_cells_to_win = 6;
+var player_symbol = 'ex';
 
 //***************************** MICAH SECTION  *******************************//
 //TODO 1. players will enter name into input field + click submit, click function will hide the player's name and append a larger name with glowing animation to indicate player's turn.
@@ -32,31 +33,69 @@ var num_of_cells_to_win;
         function animate_name (player_name) {
         }//end of animate_name function
 
-    });//end document ready
+
 //TODO 2. using jquery to capture and store as variables the number of rows and columns for the board
     //TODO disable inputs after select!! IMPORTANT , need RESET BUTTON
-        // capture num_of_rows
-        function capture_number_of_rows_input(){
-            //capture num_of_rows store as var
-            num_of_rows = $('.number_of_rows').val();
-            console.log('num_of_rows = ' + num_of_rows);
-            num_of_rows = parseInt(num_of_rows);
-        }//end capture function
+    //     // capture num_of_rows
+    //     function capture_number_of_rows_input(){
+    //         //capture num_of_rows store as var
+    //         num_of_rows = $('.number_of_rows').val();
+    //         console.log('num_of_rows = ' + num_of_rows);
+    //         num_of_rows = parseInt(num_of_rows);
+    //     }//end capture function
 
 //TODO 3. using jquery to capture and store as variable the number of matches the player chooses, this will be the win condition parameter.
-        // capture num_of_cells_to_win
-        function capture_num_of_cells_input(){
-            //capture num_of_cells_to_win store as var
-            num_of_cells_to_win = $('.number_of_matches').val();
-            console.log('num_of_cells_to_win_input = ' + num_of_cells_to_win);
-            num_of_cells_to_win = parseInt(num_of_cells_to_win);
-        }//end capture function
+//         // capture num_of_cells_to_win
+//         function capture_num_of_cells_input(){
+//             //capture num_of_cells_to_win store as var
+//             num_of_cells_to_win = $('.number_of_matches').val();
+//             console.log('num_of_cells_to_win_input = ' + num_of_cells_to_win);
+//             num_of_cells_to_win = parseInt(num_of_cells_to_win);
+//         }//end capture function
 
 //TODO 4. using jquery to dynamically create game board based on user input
         //
+        /////dynamic creation
+        for(var x = 0;x <= num_of_rows-1; x++){
+            for(var y = 0;y <= num_of_rows-1; y++) {
+                var new_obj =
+                {
+                    row : x,
+                    col : y,
+                    html: $("<div class='cell'></div>"),
+                    click_handler: function(){
+                        var symbol = toggle_and_get_current_symbol();
+                        this.html.addClass('clicked '+symbol);
 
+                    }//click handler
+                };///new obj
+                make_click(new_obj);
+                $(".game_board").append(new_obj.html);
+            }////y
+        }////x
 
+        //TODO width and height of cell
+        var cell_width = 95/num_of_rows+'vh';
 
+        $('.cell').css({"width": cell_width,"height": cell_width});
+
+        function make_click(the_object){
+            the_object.html.click(function(){
+                console.log('object that was triggered',the_object);
+                the_object.click_handler();
+            });
+        }
+
+        function toggle_and_get_current_symbol(){
+            if(player_symbol=='ex'){
+                player_symbol = 'ow';
+            }
+            else{
+                player_symbol = 'ex';
+            }
+            return player_symbol;
+        }
+    });//end document ready
 //***************************** PEARL SECTION  *******************************//
 
 
