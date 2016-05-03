9bca9f30549475960ad0ccbe9fc25663c5aadea6
Tevin Mantock <tmantock@gmail.com>
2016-05-03 14:32:00 -0700

    commented

--- dynamic.js
+++ dynamic.js
@@ -1,54 +1,42 @@
 /**
  * Created by Mantock on 5/3/16.
  */
-//Global variable for cube generation
-
-
-//global variable for three row creation
-
-
-//global variable for ten row creation
-//var gameRowTen;
-
-//global variable for 15 row creation
-//var gameRowFifteen;
-
-//global variable for 20 row creation
-//var gameRowTwenty;
-
-//Global variable for user input
 var userGameBoardInput = prompt('What size do you wish to have? 3,10,15,20?');
 
 //Change the Global variable for input to a number value
-var userGameInputNumber = 3;
-
+var userGameInputNumber = parseInt(userGameBoardInput);
 
 $(document).ready(function () {
     
     dynamicGameAssignment();
 
 });
-
-
-
-//
-
+//function declared for dynamically creating tic tac rows and columns
 function dynamicGameAssignment () {
+    //variable declared for counting the rows assigned to each tile
+    var rowCounter = 0;
+    //for loop started for moving through rows
     for(i=0;i<userGameInputNumber;i++) {
+        //local variable for game tiles declared
         var gameTile;
+        //local variable for game rows declared
         var gameRow;
-
+        //local variable for column counter declared for getting the position of an element
+        var columnCounter = 0;
+        //Series of declarations for determining which size to use for the rows and the columns
         if (userGameBoardInput == '3') {
+            //set gameTile variable to a new DOM element
             gameTile = $('<div>').attr({
-                onclick: 'getTileValue',
+                onclick: 'getTileValue(this)',
                 data: 'null'
             }).css({
                 'border': '1px solid black',
+                'box-sizing': 'border-box',
                 'height': '100%',
                 'width': '33%',
                 'display': 'inline-block'
             });
-
+            //set gameRow variable to a new DOM element
             gameRow = $('<div>').css({
                 'border': '1px solid black',
                 'height': '33%',
@@ -58,10 +46,11 @@ function dynamicGameAssignment () {
 
         else if (userGameBoardInput == '10') {
             gameTile = $('<div>').attr({
-                onclick: 'getTileValue',
+                onclick: 'getTileValue(this)',
                 data: 'null'
             }).css({
                 'border': '1px solid black',
+                'box-sizing': 'border-box',
                 'height': '100%',
                 'width': '10%',
                 'display': 'inline-block'
@@ -76,10 +65,11 @@ function dynamicGameAssignment () {
 
         else if (userGameBoardInput == '15') {
             gameTile = $('<div>').attr({
-                onclick: 'getTileValue',
+                onclick: 'getTileValue(this)',
                 data: 'null'
             }).css({
                 'border': '1px solid black',
+                'box-sizing': 'border-box',
                 'height': '100%',
                 'width': '7%',
                 'display': 'inline-block'
@@ -94,10 +84,12 @@ function dynamicGameAssignment () {
 
         else if (userGameBoardInput == '20') {
             gameTile = $('<div>').attr({
-                onclick: 'getTileValue',
-                data: 'null'
+                onclick: 'getTileValue(this)',
+                data: 'null',
+                data_position:[columnCounter,rowCounter]
             }).css({
                 'border': '1px solid black',
+                'box-sizing': 'border-box',
                 'height': '100%',
                 'width': '5%',
                 'display': 'inline-block'
@@ -111,31 +103,22 @@ function dynamicGameAssignment () {
 
         }
         console.log(gameTile);
+        //Nested for loop for moving through the row
         for(e=0;e<userGameInputNumber;e++) {
             console.log('I am e ' + e);
-            $(gameRow).append(gameTile.clone());
+            //append the gameTiles to the gameRow with attributes for getting the position
+            $(gameRow).append(gameTile.clone().attr({
+                'data-column': columnCounter,
+                'data-row': rowCounter}));
             console.log(this+' I am tile.');
+            //increment the columnCounter
+            columnCounter++;
         }
+        //Append the newly created rown to the gameBoard
         $(".gameBoard").append(gameRow.clone());
+        //increment the rowCounter
+        rowCounter++;
+
         // console.log('I am i ' + i);
     }
 }
-
-
-// function test () {
-//     var tile = gameTile;
-// }
-
-// var realTile = new test();
-
-// function tileCreation () {
-//     for(i=0;i<userGameInputNumber;i++) {
-//         $(".gameBoard").append(gameRow.clone());
-//         console.log('I am i ' + i);
-//         for(e=0;e<userGameInputNumber;e++) {
-//             console.log('I am e ' + e);
-//             $(gameRow).append(gameTile.clone());
-//             console.log(this+' I am tile.');
-//         }
-//     }
-// }
