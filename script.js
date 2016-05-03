8429ba29ca74d705dae7ef3a716c8fa61fb93075
Morasthite <Morasthite@gmail.com>
2016-05-03 13:52:15 -0700

    begin work on final part, after pc crash

---
+++ script.js
@@ -0,0 +1,20 @@
+//***************************** MICAH SECTION  *******************************//
+//TODO 1. players will enter name into input field + click submit, click function will hide the player's name and append a larger name with glowing animation to indicate player's turn.
+// CLICK HANDLER FOR PLAYER NAME SUBMIT BUTTON
+    $(document).ready(function(){
+        $("#player1-name").click(function(){
+
+        });//end 
+    });//end document ready
+//TODO 2. using jquery to capture and store as variables the number of rows and columns for the board
+//TODO 3. using jquery to capture and store as variable the number of matches the player chooses, this will be the win condition parameter.
+
+
+
+//***************************** PEARL SECTION  *******************************//
+
+
+
+
+
+//***************************** AMINA SECTION  *******************************//
