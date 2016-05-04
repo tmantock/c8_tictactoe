01ab12d6cf931d69542e65df07ee12c0d58f747a
Morasthite <Morasthite@gmail.com>
2016-05-03 20:55:14 -0700

    styling mostly done

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var num_of_rows=3;
 var num_of_cells_to_win=3;
 var player_symbol = 'ex';
 var grid_array = [];
+var flag_to_move = true;// if player's turn to move, this flag must be true.
 
 //***************************** MICAH SECTION  *******************************//
 //TODO 1. players will enter name into input field + click submit, click function will hide the player's name and append a larger name with glowing animation to indicate player's turn.
@@ -18,13 +19,13 @@ $(document).ready(function(){
         //capture plyr name store as var
         player1_name_value = $('#player1-name-value').val();
         console.log('var player1_name_value = ' + player1_name_value);
-        // hide player input fields, append big name
+        // hide player input fields, append player name
         $('#player1-name-value').hide();
         $('#player1-name-submit').hide();
         var local_player1_name_value = player1_name_value;
         $('.player.player1').addClass('appended');
         $('.player.player1.appended').append(local_player1_name_value);
-        //local_player1_name_value.appendTo('player player2');
+        animate_name();
     });//end click
 
     //Player2-name-value capture
@@ -32,34 +33,45 @@ $(document).ready(function(){
         //capture plyr nam store as var
         player2_name_value = $('#player2-name-value').val();
         console.log('var player2_name_value = ' + player2_name_value);
+        $('#player2-name-value').hide();
+        $('#player2-name-submit').hide();
+        var local_player2_name_value = player2_name_value;
+        $('.player.player2').addClass('appended');
+        $('.player.player2.appended').append(local_player2_name_value);
+        animate_name();
     });//end click
 
     // Player name animation function
-    function animate_name (player_name) {
+    function animate_name () {
+        if(flag_to_move === true) {
+        $('.player.appended').addClass('animate');
+        }
     }//end of animate_name function
 
 
-//TODO 2. using jquery to capture and store as variables the number of rows and columns for the board
-    //TODO disable inputs after select!! IMPORTANT , need RESET BUTTON
-    //     // capture num_of_rows
-    //     function capture_number_of_rows_input(){
-    //         //capture num_of_rows store as var
-    //         num_of_rows = $('.number_of_rows').val();
-    //         console.log('num_of_rows = ' + num_of_rows);
-    //         num_of_rows = parseInt(num_of_rows);
-    //     }//end capture function
+// TODO 2. using jquery to capture and store as variables the number of rows and columns for the board
+//     TODO disable inputs after select!! IMPORTANT , need RESET BUTTON
+        // capture num_of_rows
+        function capture_number_of_rows_input(){
+            //capture num_of_rows store as var player2_name_value = $('#player2-name-value').val();
+            num_of_rows = $('.number_of_rows').val();
+            console.log('num_of_rows = ' + num_of_rows);
+            $('.row-number').hide();
+            num_of_rows = parseInt(num_of_rows);
+        }//end capture function
 
-//TODO 3. using jquery to capture and store as variable the number of matches the player chooses, this will be the win condition parameter.
-//         // capture num_of_cells_to_win
-//         function capture_num_of_cells_input(){
-//             //capture num_of_cells_to_win store as var
-//             num_of_cells_to_win = $('.number_of_matches').val();
-//             console.log('num_of_cells_to_win_input = ' + num_of_cells_to_win);
-//             num_of_cells_to_win = parseInt(num_of_cells_to_win);
-//         }//end capture function
+// TODO 3. using jquery to capture and store as variable the number of matches the player chooses, this will be the win condition parameter.
+        // capture num_of_cells_to_win
+        function capture_num_of_cells_input(){
+            //capture num_of_cells_to_win store as var
+            num_of_cells_to_win = $('.number_of_matches').val();
+            console.log('num_of_cells_to_win_input = ' + num_of_cells_to_win);
+            $('.number_of_matches').hide();
+            num_of_cells_to_win = parseInt(num_of_cells_to_win);
+        }//end capture function
 
-//TODO 4. using jquery to dynamically create game board based on user input
-    ////creating the array grid
+// TODO 4. using jquery to dynamically create game board based on user input
+//     //creating the array grid
     create_grid_array();
     /////dynamic creation of game board
     for(var x = 0;x <= num_of_rows-1; x++){
